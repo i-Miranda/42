e61c221c38f42b5dfee02547725ec346b123fabb
i-Miranda <ivanmirandaart@yahoo.com>
2024-08-14 07:35:39 +0200

    adding more exs

---
+++ c00/ex05/ft_print_comb.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+void	ft_print_comb(void)
+{
+	char	first;
+	char	second;
+	char	third;
+
+	first = '0';
+	second = '0';
+	third = '0';
+	while (first < '8')
+	{
+		second = first + 1;
+		while (second < '9')
+		{
+			third = second + 1;
+			while (third < ':')
+			{
+				write(1, &first, 1);
+				write(1, &second, 1);
+				write(1, &third, 1);
+				if (first + second + third != '7' + '8' + '9')
+					write(1, ", ", 1);
+			}
+			second++;
+		}
+		first++;	
+	}
+}
