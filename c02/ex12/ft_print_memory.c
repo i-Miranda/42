8ab119df90d69465965160dadb251a6f78bcdc76
i-Miranda <ivanmirandaart@yahoo.com>
2024-08-18 00:31:54 +0200

    Cleaning up print memory.  Still cant print memory address of addr...

--- c02/ex12/ft_print_memory.c
+++ c02/ex12/ft_print_memory.c
@@ -39,15 +39,49 @@ void	ft_print_hexvalue(int decval)
 	}
 }
 
+void	ft_print_substr(char *str, unsigned int length, unsigned int index)
+{
+	unsigned int	j; 
+
+	j = 0;
+	while (j < 16 && index + j < length)
+	{
+		if (str[index + j] <= 31)
+			write(1, ".", 1);
+		else
+			write(1, &str[index + j], 1);
+		j++;
+	}
+}
+
+void	ft_print_substr_hexval(char *str, unsigned int length,
+		unsigned int index)
+{
+	unsigned int	j;
+
+	j = 0;
+	while (j < 16)
+	{
+		if (index + j < length)
+			ft_print_hexvalue(str[index + j]); 
+		else
+			write(1, "  ", 2);
+		if ((index + j) % 2 != 0)
+			write(1, " ", 1);
+		j++;
+	}
+}
+
 void	*ft_print_memory(void *addr, unsigned int size)
 {
 	if (size > 0)
 	{
-		unsigned int i;
-		unsigned int j;
-		char *chr_addr = addr;
+		unsigned int	i;
+		unsigned int	j;
+		char			*chr_addr;
 
 		i = 0;
+		chr_addr = addr;
 		while(i < size)
 		{
 			j = 0;
@@ -57,28 +91,10 @@ void	*ft_print_memory(void *addr, unsigned int size)
 				j++;
 			}
 			write(1, ": ", 2);
-			j = 0;
-			while (j < 16)
-			{
-				if (i + j < size)
-					ft_print_hexvalue(chr_addr[i + j]); 
-				else
-					write(1, "  ", 2);
-				if ((i + j) % 2 != 0)
-					write(1, " ", 1);
-				j++;
-			}
-			j = 0;
-			while (j < 16 && i + j < size)
-			{
-				if (chr_addr[i + j] <= 31)
-					write(1, ".", 1);
-				else
-					write(1, &chr_addr[i + j], 1);
-				j++;
-			}
+			ft_print_substr_hexval(chr_addr, size, i);
+			ft_print_substr(chr_addr, size, i);
 			write(1, "\n", 1);
-			i = i + 16;	
+			i += 16;	
 		}
 	}
 	return (addr);
@@ -88,6 +104,7 @@ int	main(void)
 {
 	char test[] = "Bonjour les aminches\t\n\tc\a est fou\ttout\tce qu on peut"
 		" faire avec\t\n\tprint_memory\n\n\n\tlol.lol\n \0";
+
 	ft_print_memory(test, ft_strlen(test) + 1);
 	return (0);
 }
