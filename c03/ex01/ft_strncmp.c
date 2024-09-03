6b7d075146a01df24a27bfdd954d8ef7923865c8
i-Miranda <ivanmirandaart@yahoo.com>
2024-09-03 16:32:29 +0200

    fixed c03ex01, ft_strncmp.c

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -36,24 +36,49 @@ void	ft_putstr(char *str)
 	}
 }	
 
+int	ft_atoi(char *str)
+{
+	int i;
+	int atoi[2];
+
+	i = 0;
+	atoi[0] = 0;
+	atoi[1] = 1;
+	while ((str[i] != '\0') && (str[i] == ' '))
+		i++;
+	while ((str[i] != '\0') && (str[i] == '-' || str[i] == '+'))
+	{
+		if (str[i] == '-')
+			atoi[1] *= -1;	
+		i++;
+	}
+	while ((str[i] != '\0') && (str[i] >= '0' && str[i] <= '9'))
+	{
+		atoi[0] = atoi[0] * 10 + str[i] - '0';
+		i++;
+	}
+	return (atoi[0] * atoi[1]);	
+}
+
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
 
 	i = 0;
-	while (i < n && s1[i] != '\0' && s1[i] == s2[i])
+	while (i < n - 1 && s1[i] != '\0' && s1[i] == s2[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
 int	main(int argc, char **argv)
 {
-	if (argc == 3)
+	if (argc == 4)
 	{
 		ft_putstr(argv[1]);			
 		write(1, " + ", 3);
 		ft_putstr(argv[2]);			
 		write(1, " : ", 3);
-		ft_putnbr(ft_strcmp(argv[1], argv[2]));
+		ft_putnbr(ft_strncmp(argv[1], argv[2], 
+								(unsigned int)ft_atoi(argv[3])));
 	}
 }
