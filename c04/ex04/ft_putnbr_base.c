28b022d508d244b243050962616907350139edd3
i-Miranda <ivanmirandaart@yahoo.com>
2024-09-03 19:57:05 +0200

    continued with ft _putnbr_base.c

--- c04/ex04/ft_putnbr_base.c
+++ c04/ex04/ft_putnbr_base.c
@@ -5,7 +5,7 @@ int	ft_strlen(char *str)
 	int i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (str[i])
 		i++;
 	return (i);
 }
@@ -48,7 +48,7 @@ void    ft_check_for_negative(int *nb, char *base)
             write(1, "-", 1);
         else
         {
-            result = (1 << 31);
+            result = 1 << 31;
             result -= 1;
             *nb = ~*nb & result;
             *nb ^= 1;
@@ -62,7 +62,7 @@ void	ft_putnbr_base(int nb, char *base)
 
     i = 0;
 	if (ft_check_base(base) == 1)
-		return ;
+		return;
     ft_check_for_negative(&nb, base);
     if (nb > ft_strlen(base) - 1)
     {
@@ -105,14 +105,14 @@ int	main(void)
     write(1, "\n\tin octal: ", 12);
 	ft_putnbr_base(2147483647, "01234567");
 	write(1, "\n", 1);
-    write(1, "Testing -2147483647", 19);
+    write(1, "Testing -2147483648", 19);
     write(1, "\n\tin decimal: ", 14);
-	ft_putnbr_base(-2147483647, "0123456789");
+	ft_putnbr_base(-2147483648, "0123456789");
     write(1, "\n\tin binary: ", 13);
-	ft_putnbr_base(-2147483647, "01");
+	ft_putnbr_base(-2147483648, "01");
     write(1, "\n\tin hexadecimal: ", 18);
-	ft_putnbr_base(-2147483647, "0123456789abcdef");
+	ft_putnbr_base(-2147483648, "0123456789abcdef");
     write(1, "\n\tin octal: ", 12);
-	ft_putnbr_base(-2147483647, "01234567");
+	ft_putnbr_base(-2147483648, "01234567");
 	write(1, "\n", 1);
 }
