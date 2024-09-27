c21582a85daabfc5486bf550473e3c8e3f2e02aa
i-Miranda <ivanmirandaart@yahoo.com>
2024-09-27 23:41:50 +0200

    fixed strjoin and strtrim

--- crss/libft/ft_strjoin.c
+++ crss/libft/ft_strjoin.c
@@ -15,13 +15,15 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
+	size_t	s1_len;
+	size_t	s2_len;
 
-	str = malloc((ft_strlen(s1) + ft_strlen(s2)) * sizeof(char));
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	str = ft_calloc((s1_len + s2_len + 1), sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	while (*s1 != '\0')
-		*str++ = *s1++;
-	while (*s2 != '\0')
-		*str++ = *s2++;
+	ft_memmove(str, s1, s1_len);
+	ft_memmove(&str[s1_len], s2, s2_len);
 	return (str);
 }
--- crss/libft/ft_strtrim.c
+++ crss/libft/ft_strtrim.c
@@ -14,7 +14,6 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	i;
 	size_t	start;
 	size_t	end;
 	char	*str;
@@ -25,14 +24,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 		start++;
 	while (ft_strrchr(set, s1[end]) != NULL && end > 0)
 		end--;
-	str = malloc((end - start) * sizeof(char));
-	if (str == NULL)
-		return (NULL);
-	i = 0;
-	while (i < (end - start))
-	{
-		str[i] = s1[start + i];
-		i++;
-	}
+	str = ft_substr(s1, start, end - start + 1);
 	return (str);
 }
