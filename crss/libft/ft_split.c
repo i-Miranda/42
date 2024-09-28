41b700795a0aaef9557f10898e486014c3fb8dca
Ivan Miranda Moral <ivmirand@c1r13s5.42madrid.com>
2024-09-28 16:13:39 +0200

    updated ft_strrchr and ft_split.c

--- crss/libft/ft_split.c
+++ crss/libft/ft_split.c
@@ -6,65 +6,71 @@
 /*   By: ivmirand <ivmirand@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 12:08:46 by ivmirand          #+#    #+#             */
-/*   Updated: 2024/09/28 11:07:58 by ivmirand         ###   ########.fr       */
+/*   Updated: 2024/09/28 16:13:09 by ivmirand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	ft_charcount(char const *str, char c)
+static int	ft_wordcount(char const *str, char c)
 {
 	int		i;
-	char	*previous;
+	int		chr_match;
 	int		len;
-	size_t	word_count;
+	int		word_count;
 
 	i = 0;
-	previous = 0;
+	chr_match = 0;
 	word_count = 0;
 	len = ft_strlen(str);
 	while (i < len)
 	{
-		if (str[i] == c && previous != (char *)&str[i])
+		if (str[i] != c && chr_match == 0)
 		{
 			word_count++;
-			previous = (char *)&str[i];
+			chr_match = 1;
 		}
+		else if (str[i] == c)
+			chr_match = 0;
 		i++;
 	}
 	return (word_count);
 }
 
+static void	*ft_freeptr(char **strarray, int count)
+{
+	while (count--)
+		free(strarray[count]);
+	free(strarray[count]);
+	return (NULL);
+}
+
 char	**ft_split(char const *s, char c)
 {
+	int		i;
+	int		build_word;
 	size_t	count;
-	size_t	i;
 	size_t	len;
 	char	**strarray;
 
 	if (s == NULL)
 		return (NULL);
-	count = 0;
 	i = 0;
+	count = 0;
+	build_word = -1;
 	len = ft_strlen(s);
 	while (i++ < len)
-		count += ft_charcount(&s[i], c);
+		count += ft_wordcount(&s[i], c);
 	strarray = ft_calloc(count + 1, sizeof(char *));
 	if (strarray != NULL)
 		return (strarray);
-	if (count == 0)
-		strarray[0] = ft_strdup(s);
-	else
+	i = 0;
+	while (i++ <= len)
 	{
-		i = 0;
-		len = 0;
-		while (strarray[i++] != NULL)
-		{
-			strarray[i] = ft_substr(s, len, ft_charcount(&s[len], c));
-			if (strarray[i] == NULL)
-				free(strarray[i]);
-			len += ft_charcount(&s[len], c);
-		}
+		strarray[i] = ft_substr(s, len, ft_wordcount(&s[len], c));
+		if (strarray[i] == NULL)
+			free(strarray[i]);
+		len += ft_wordcount(&s[len], c);
 	}
 	return (strarray);
 }
--- crss/libft/ft_strrchr.c
+++ crss/libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ivmirand <ivmirand@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 12:09:24 by ivmirand          #+#    #+#             */
-/*   Updated: 2024/09/28 13:30:42 by ivmirand         ###   ########.fr       */
+/*   Updated: 2024/09/28 15:08:33 by ivmirand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,10 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	size_t	len;
-	char	*str;
 
 	len = ft_strlen(s) + 1;
-	str = (char *)s;
 	while (len--)
-		if (str[len] == c)
-			return (&str[len]);
+		if (s[len] == c)
+			return ((char *)&s[len]);
 	return (NULL);
 }
