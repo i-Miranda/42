c6657c5164692af168b98b1c6ad383759419a4de
Ivan Miranda Moral <ivmirand@c2r3s4.42madrid.com>
2024-09-28 11:09:29 +0200

    more ft_split changes, still too many lines, trying not to add a second static function

--- crss/libft/ft_split.c
+++ crss/libft/ft_split.c
@@ -6,13 +6,13 @@
 /*   By: ivmirand <ivmirand@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 12:08:46 by ivmirand          #+#    #+#             */
-/*   Updated: 2024/09/28 10:51:37 by ivmirand         ###   ########.fr       */
+/*   Updated: 2024/09/28 11:07:58 by ivmirand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_charcount(char const *str, char c)
+static size_t	ft_charcount(char const *str, char c)
 {
 	int		i;
 	int		previous;
@@ -44,25 +44,23 @@ char	**ft_split(char const *s, char c)
 	count = 0;
 	i = 0;
 	len = ft_strlen(s);
-	while (i < len)
-		count += ft_charcount(&s[i++], c);
+	while (i++ < len)
+		count += ft_charcount(&s[i], c);
 	strarray = ft_calloc(count + 1, sizeof(char *));
 	if (strarray != NULL)
+		return (strarray);
+	if (count == 0)
+		strarray[0] = ft_strdup(s);
+	else
 	{
-		if (count == 0)
-			strarray[0] = ft_strdup(s);
-		else
+		i = 0;
+		len = 0;
+		while (strarray[i++] != NULL)
 		{
-			i = 0;
-			len = 0;
-			while (strarray[i] != NULL)
-			{
-				strarray[i] = ft_substr(s, len, ft_charcount(&s[len], c));
-				len = ft_charcount(&s[len], c);
-				if (strarray[i] == NULL)
-					free(strarray[i]);
-				i++;
-			}
+			strarray[i] = ft_substr(s, len, ft_charcount(&s[len], c));
+			if (strarray[i] == NULL)
+				free(strarray[i]);
+			len += ft_charcount(&s[len], c);
 		}
 	}
 	return (strarray);
