844eb1241c9723a5c70ec29b3e380b612a0e2895
Ivan Miranda Moral <ivmirand@c1r13s5.42madrid.com>
2024-09-28 13:47:22 +0200

    changed strrchr again

--- crss/libft/ft_strrchr.c
+++ crss/libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ivmirand <ivmirand@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 12:09:24 by ivmirand          #+#    #+#             */
-/*   Updated: 2024/09/28 13:20:11 by ivmirand         ###   ########.fr       */
+/*   Updated: 2024/09/28 13:30:42 by ivmirand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strrchr(const char *s, int c)
 	size_t	len;
 	char	*str;
 
-	len = ft_strlen(s);
+	len = ft_strlen(s) + 1;
 	str = (char *)s;
 	while (len--)
 		if (str[len] == c)
