168e5d9e8820f4941aba30c18f9ed4c9c1b14394
Ivan Miranda Moral <ivmirand@c2r3s4.42madrid.com>
2024-09-28 10:52:02 +0200

    updated ft_split, but still not fixed

--- crss/libft/ft_split.c
+++ crss/libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: ivmirand <ivmirand@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 12:08:46 by ivmirand          #+#    #+#             */
-/*   Updated: 2024/09/17 12:48:38 by ivmirand         ###   ########.fr       */
+/*   Updated: 2024/09/28 10:51:37 by ivmirand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 static int	ft_charcount(char const *str, char c)
 {
-	int	i;
-	int	previous;
-	int	word_count;
+	int		i;
+	int		previous;
+	size_t	word_count;
+	size_t	len;
 
 	i = 0;
 	previous = 0;
 	word_count = 0;
-	while (str[i] != '\0')
+	len = ft_strlen(str);
+	while (i < len)
 	{
 		if (str[i] == c && previous != c)
 			word_count++;
@@ -32,7 +34,7 @@ static int	ft_charcount(char const *str, char c)
 
 char	**ft_split(char const *s, char c)
 {
-	int		count;
+	size_t	count;
 	int		i;
 	int		len;
 	char	**strarray;
@@ -41,25 +43,26 @@ char	**ft_split(char const *s, char c)
 		return (NULL);
 	count = 0;
 	i = 0;
-	while (s[i] != '\0')
+	len = ft_strlen(s);
+	while (i < len)
 		count += ft_charcount(&s[i++], c);
 	strarray = ft_calloc(count + 1, sizeof(char *));
 	if (strarray != NULL)
 	{
 		if (count == 0)
-		{
 			strarray[0] = ft_strdup(s);
-			return (strarray);
-		}
-		i = 0;
-		len = 0;
-		while (strarray[i] != NULL)
+		else
 		{
-			strarray[i] = ft_substr(s, len, (size_t)ft_charcount(&s[len], c));
-			len = ft_charcount(&s[len], c);
-			if (strarray[i] == NULL)
-				free(strarray[i]);
-			i++;
+			i = 0;
+			len = 0;
+			while (strarray[i] != NULL)
+			{
+				strarray[i] = ft_substr(s, len, ft_charcount(&s[len], c));
+				len = ft_charcount(&s[len], c);
+				if (strarray[i] == NULL)
+					free(strarray[i]);
+				i++;
+			}
 		}
 	}
 	return (strarray);
