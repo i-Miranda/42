e65c77ccf02f844ee373cd1c55706e0c5d5e2833
Ivan Miranda Moral <ivmirand@c2r3s4.42madrid.com>
2024-09-28 13:20:42 +0200

    testing ft_strrchr.c

--- crss/libft/ft_strrchr.c
+++ crss/libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ivmirand <ivmirand@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 12:09:24 by ivmirand          #+#    #+#             */
-/*   Updated: 2024/09/28 13:05:39 by ivmirand         ###   ########.fr       */
+/*   Updated: 2024/09/28 13:20:11 by ivmirand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,13 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
+	size_t	len;
 	char	*str;
 
-	i = 0;
+	len = ft_strlen(s);
 	str = (char *)s;
-	while (str[i])
-	{
-		if (str[i] == c)
-			return (&str[i]);
-		i++;
-	}
+	while (len--)
+		if (str[len] == c)
+			return (&str[len]);
 	return (NULL);
 }
