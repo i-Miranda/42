674fdb268755737745dd0ab74067a9eb75655004
i-Miranda <ivanmirandaart@yahoo.com>
2024-11-08 20:28:23 +0100

    removed stdio from everything except main

--- crss/get_next_line/get_next_line.c
+++ crss/get_next_line/get_next_line.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
-#include <stdio.h>
 
 static t_list	*ft_after_nl(t_list **list)
 {
--- crss/get_next_line/main.c
+++ crss/get_next_line/main.c
@@ -12,6 +12,7 @@
 
 #include "get_next_line.h"
 #include <fcntl.h>
+#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
