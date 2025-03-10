154fce81f60ca0373b2cd24a75bbc716cd7be7bb
i-Miranda <ivanmirandaart@yahoo.com>
2025-03-10 11:22:30 +0100

    added esc key to fdf to close. also changed main.c to fdf.c

--- crss/fdf/src/input.c
+++ crss/fdf/src/input.c
@@ -6,7 +6,7 @@
 /*   By: ivmirand <ivmirand@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 09:09:36 by ivmirand          #+#    #+#             */
-/*   Updated: 2025/03/08 09:13:50 by ivan             ###   ########.fr       */
+/*   Updated: 2025/03/10 11:15:23 by ivan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,25 @@ void	input_hook(void *mlx_param)
 	mlx_t *mlx;
 	if (mlx_param == NULL)
 	{
-		puts("input_hook param is null\n");
+		ft_printf("input_hook param is null\n");
 		return ;
 	}
 		mlx = (mlx_t *)mlx_param; 
 	if (mlx_is_key_down(mlx, MLX_KEY_W) || mlx_is_key_down(mlx, MLX_KEY_UP)
 			|| mlx_is_key_down(mlx, MLX_KEY_K))
-		puts("PRESSING UP\n");
+		ft_printf("PRESSING UP\n");
 	if (mlx_is_key_down(mlx, MLX_KEY_S) || mlx_is_key_down(mlx, MLX_KEY_DOWN)
 			|| mlx_is_key_down(mlx, MLX_KEY_J))
-		puts("PRESSING DOWN\n");
+		ft_printf("PRESSING DOWN\n");
 	if (mlx_is_key_down(mlx, MLX_KEY_A) || mlx_is_key_down(mlx, MLX_KEY_LEFT)
 			|| mlx_is_key_down(mlx, MLX_KEY_H))
-		puts("PRESSING LEFT\n");
+		ft_printf("PRESSING LEFT\n");
 	if (mlx_is_key_down(mlx, MLX_KEY_D) || mlx_is_key_down(mlx, MLX_KEY_RIGHT)
 			|| mlx_is_key_down(mlx, MLX_KEY_L))
-		puts("PRESSING RIGHT\n");
+		ft_printf("PRESSING RIGHT\n");
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+	{
+		ft_printf("EXITING FDF\n");
+		mlx_close_window(mlx);
+	}
 }
